e3fe5e8e71b722c8481533a3be12e05a89546101
Mitchell Hewes <me@mitcdh.com>
2023-02-14 15:27:16 +0000

    Support displaying fullnames

--- cb2geochart.js
+++ cb2geochart.js
@@ -12,7 +12,7 @@
     })
     .catch(error => console.error(error));
 
-  function convertToAlpha2(country) {
+  function getCountryRecord(country) {
     const countryUpperCase = country.toUpperCase();
     const countryLowerCase = country.toLowerCase();
   
@@ -20,7 +20,7 @@
     for (let i = 0, len = countries.length; i < len; i++) {
       const countryRecord = countries[i];
       if (countryRecord['alpha_2'] === countryUpperCase || countryRecord['alpha_3'] === countryUpperCase) {
-        return countryRecord['alpha_2'];
+        return countryRecord;
       }
     }
   
@@ -28,7 +28,7 @@
     for (let i = 0, len = countries.length; i < len; i++) {
       const countryRecord = countries[i];
       if (countryRecord['name'].toLowerCase() === countryLowerCase) {
-        return countryRecord['alpha_2'];
+        return countryRecord;
       }
     }
   
@@ -38,7 +38,7 @@
       for (let j = 0, altLen = countryRecord['alternative_names'].length; j < altLen; j++) {
         const alternativeName = countryRecord['alternative_names'][j];
         if (alternativeName === country.normalize("NFKD").toLowerCase()) {
-          return countryRecord['alpha_2'];
+          return countryRecord;
         }
       }
     }
@@ -50,8 +50,9 @@
     var countryCounts = {};
 
     for (var i = 0; i < list.length; i++) {
-      var alpha2 = convertToAlpha2(list[i]);
-      if (alpha2) {
+      var countryRecord = getCountryRecord(list[i]);
+      if (countryRecord) {
+        var alpha2 = countryRecord.alpha_2;
         if (!countryCounts[alpha2]) {
           countryCounts[alpha2] = 0;
         }
@@ -60,22 +61,46 @@
     }
 
     for (var country in countryCounts) {
-      countryData.push([country, countryCounts[country]]);
+      countryData.push([country, countryCounts[country], getCountryRecord(country).name]);
     }
-    countryData.unshift(['Country', 'Count']);
     return countryData;
   }
 
-  function drawRegionsMap(countryData) {
-    var data = google.visualization.arrayToDataTable(countryData);
-    var options = {};
-    var chart = new google.visualization.GeoChart(document.getElementById('output'));
+  // function drawRegionsMap(countryData) {
+  //   var data = google.visualization.arrayToDataTable(countryData);
+  //   var options = {};
+  //   var chart = new google.visualization.GeoChart(document.getElementById('output'));
 
-    google.visualization.events.addListener(chart, 'ready', function () {
-      download.innerHTML = '<a download="chart.png" href="' + chart.getImageURI() + '">Download as PNG</a>';
+    // google.visualization.events.addListener(chart, 'ready', function () {
+    //   download.innerHTML = '<a download="chart.png" href="' + chart.getImageURI() + '">Download as PNG</a>';
+    // });
+
+  //   chart.draw(data, options);
+  // }
+
+  // https://stackoverflow.com/questions/11098153/geochart-using-iso-3266-2-region-code-but-show-real-name
+  function drawRegionsMap(countryData) {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Country');
+    data.addColumn('number', 'Count');
+    data.addColumn('string', 'Name');
+
+    data.addRows(countryData);
+
+    var geochart = new google.visualization.GeoChart(document.getElementById('output'));
+
+    var formatter = new google.visualization.PatternFormat('{1}');  
+    formatter.format(data, [0, 2]);
+
+    var view = new google.visualization.DataView(data);
+    view.setColumns([0, 1]);  
+
+    google.visualization.events.addListener(geochart, 'ready', function () {
+      download.innerHTML = '<a download="chart.png" href="' + geochart.getImageURI() + '">Download as PNG</a>';
     });
 
-    chart.draw(data, options);
+    var options = {};
+    geochart.draw(view, options);
   }
 
   // http://stackoverflow.com/questions/2176861/javascript-get-clipboard-data-on-paste-event-cross-browser
@@ -101,7 +126,7 @@
         var html = pastebin.innerText;
         var countryList = html.split(/\r?\n/);
         var countryData = generateCountryData(countryList);
-        console.info(countryData);
+        console.log(countryData);
         wrapper.classList.remove('d-none');
 
         drawRegionsMap(countryData);
