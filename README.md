3ae04b8f9a2b5b7907566b1e904c357eefa39def
Mitchell Hewes <me@mitcdh.com>
2023-02-14 12:07:37 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# cb2geochart
+
+## Usage
+
+1. Copy the new line delimited list of country names to the clipboard.
+2. Open [index.html](index.html) served from a webserver (e.g. `python3 -m http.server`) favorite browser and paste.
+3. Browse the data in the GeoChart and download as a PNG image if needed.
+
+## Credits
+
+* Normalisation of country names is done based on the Wikipedia [List of alternative country names](https://en.wikipedia.org/wiki/List_of_alternative_country_names).
+* Chart rendering is using Google's [GeoChart](https://developers.google.com/chart/interactive/docs/gallery/geochart) visualisation.
+* This website theme and the overall idea for the code is based on [clipboard2markdown](https://github.com/euangoddard/clipboard2markdown).
+* The HTML template is based on [Bootstrap](http://getbootstrap.com/).
---
+++ cb2geochart.js
@@ -0,0 +1,110 @@
+(function () {
+  google.charts.load('current', {
+    'packages':['geochart'],
+  });
+
+  let countries;
+
+  fetch('countries.json')
+    .then(response => response.json())
+    .then(json => {
+      countries = json;
+    })
+    .catch(error => console.error(error));
+
+  function convertToAlpha2(country) {
+    const countryUpperCase = country.toUpperCase();
+    const countryLowerCase = country.toLowerCase();
+  
+    // Search for an exact match of the alpha-2 or alpha-3 code
+    for (let i = 0, len = countries.length; i < len; i++) {
+      const countryRecord = countries[i];
+      if (countryRecord['alpha_2'] === countryUpperCase || countryRecord['alpha_3'] === countryUpperCase) {
+        return countryRecord['alpha_2'];
+      }
+    }
+  
+    // Search for a match of the country name
+    for (let i = 0, len = countries.length; i < len; i++) {
+      const countryRecord = countries[i];
+      if (countryRecord['name'].toLowerCase() === countryLowerCase) {
+        return countryRecord['alpha_2'];
+      }
+    }
+  
+    // Search for a match of an alternative name
+    for (let i = 0, len = countries.length; i < len; i++) {
+      const countryRecord = countries[i];
+      for (let j = 0, altLen = countryRecord['alternative_names'].length; j < altLen; j++) {
+        const alternativeName = countryRecord['alternative_names'][j];
+        if (alternativeName === country.normalize("NFKD").toLowerCase()) {
+          return countryRecord['alpha_2'];
+        }
+      }
+    }
+    console.log("No match found for " + country + "");
+  }
+
+  function generateCountryData(list) {
+    var countryData = [];
+    var countryCounts = {};
+
+    for (var i = 0; i < list.length; i++) {
+      var alpha2 = convertToAlpha2(list[i]);
+      if (alpha2) {
+        if (!countryCounts[alpha2]) {
+          countryCounts[alpha2] = 0;
+        }
+        countryCounts[alpha2]++;
+      }
+    }
+
+    for (var country in countryCounts) {
+      countryData.push([country, countryCounts[country]]);
+    }
+    countryData.unshift(['Country', 'Count']);
+    return countryData;
+  }
+
+  function drawRegionsMap(countryData) {
+    var data = google.visualization.arrayToDataTable(countryData);
+    var options = {};
+    var chart = new google.visualization.GeoChart(document.getElementById('output'));
+
+    google.visualization.events.addListener(chart, 'ready', function () {
+      download.innerHTML = '<a download="chart.png" href="' + chart.getImageURI() + '">Download as PNG</a>';
+    });
+
+    chart.draw(data, options);
+  }
+
+  // http://stackoverflow.com/questions/2176861/javascript-get-clipboard-data-on-paste-event-cross-browser
+  document.addEventListener('DOMContentLoaded', function () {
+    var info = document.querySelector('#info');
+    var pastebin = document.querySelector('#pastebin');
+    var output = document.querySelector('#output');
+    var wrapper = document.querySelector('#wrapper');
+
+    document.addEventListener('keydown', function (event) {
+      if (event.ctrlKey || event.metaKey) {
+        if (String.fromCharCode(event.which).toLowerCase() === 'v') {
+          pastebin.innerHTML = '';
+          pastebin.focus();
+          info.classList.add('hidden');
+          wrapper.classList.add('hidden');
+        }
+      }
+    });
+
+    pastebin.addEventListener('paste', function () {
+      setTimeout(function () {
+        var html = pastebin.innerText;
+        var countryList = html.split(/\r?\n/);
+        var countryData = generateCountryData(countryList);
+        wrapper.classList.remove('hidden');
+
+        drawRegionsMap(countryData);
+      }, 200);
+    });
+  });
+})();
